d0ac314e7f5a3b901414c7851d4516ba12716e51
DreamSlayerGX <DreamSlayerGX@gmail.com>
2017-02-28 15:47:49 +0100

    Add files via upload
    La till kod i run som är tänkt att köra programmet tills dess slut

--- Simulator.java
+++ Simulator.java
@@ -1,5 +1,28 @@
 package simulator;
 
 public class Simulator {
+	
+	private State state;
+	private Store store;
+	
+	Simulator(){
+		run();
+	}
+	
+	public void run(){
+		while(!state.getFlag()){
+			
+		store.nextEvent();
+			
+			
+		}
+		
+	}
+	
+	public static void main(String[] args){
+		Simulator sim = new Simulator();
+		
+		
+	}
 
 }
