f0d9eee3c7ba4a9259c068769a4280d0f2dc66ac
antonkdahlin <anton.nicolaus@gmail.com>
2017-02-28 16:36:36 +0100

    First version of store
    En första version av Store. Har ingen error checkning än. Använder sig
    av event.getTime och antar att den får tillbaka en double

--- Store.java
+++ Store.java
@@ -1,19 +1,38 @@
 package simulator;
 
 import java.util.ArrayList;
-
+import java.util.Iterator;
+import java.util.LinkedList;
+/**
+ * Contains a list of events sorted by time, low to high
+ * @author anodah-6
+ *
+ */
 public class Store {
-	
-	private ArrayList<Event> eventList;
-	
+	private LinkedList<Event> eventList;
+	/**
+	 * Initializes
+	 */
+	public Store(){
+		eventList = new LinkedList;
+	}
+	/**
+	 * adds a new event to the list, placed depenting on time of the event
+	 */
 	public void storeEvent(Event event){
-		
+		int place = 0;
+		for(Event e : eventList){
+			if(event.getTime() < e.getTime()){
+				break;
+			}
+			place++;
+		}
+		eventList.add(place, event);
 	}
-	
-	public Event nextEvent(){
-		
-		return null;
+	/**
+	 * returns and removes the first event in the list
+	 */
+	public Event nextEvent(){	
+		return eventList.poll();
 	}
-	
-
 }
